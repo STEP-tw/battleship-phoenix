5c676f9101130730377584656fbdd4106cbeadd6
arvind singh <1141191arvind.singh@gamil.com>
2018-02-14 15:51:52 +0530

    [arvind] [fixed bug] changed sh file for creating log file

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,6 +5,8 @@ npm install -g nodemon
 npm install -g eslint
 apm install
 
+mkdir log
+touch log/request.log
 git config --local commit.template .github/commit.txt
 cp .github/pre-commit.sh .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
