c95f371ad927b02a662968f4dfea38e029018ac1
arvind singh <1141191arvind.singh@gamil.com>
2018-02-14 13:40:05 +0530

    [arvind] changed package.json file

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,6 +3,7 @@ npm install -g mocha
 npm install -g nyc
 npm install -g nodemon
 npm install -g eslint
+apm install
 
 git config --local commit.template .github/commit.txt
 cp .github/pre-commit.sh .git/hooks/pre-commit
