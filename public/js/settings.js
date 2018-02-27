3715e312fd0a873d8ef57acf48194c47bf153562
ruwik <manindrakrishna@gmail.com>
2018-02-27 13:15:20 +0530

    [#53] [manindra/sudhin] removed option for changing language
    What changes have you made?
    
    * removed optio for changing language
    
    Why do we need this change?
    
    * This feature was incomplete
    
    What can this change impact?
    
     This will make the game completely testable

--- public/js/settings.js
+++ public/js/settings.js
@@ -3,12 +3,8 @@ const bgmController = function() {
   musicElement.muted = !musicElement.muted;
   let listItemColor = document.getElementById('music').style;
   if (listItemColor.backgroundColor=="grey") {
-    listItemColor.backgroundColor = "white";
+    listItemColor.backgroundColor = "rgba(0, 0, 0, 0.85)";
   }else {
     listItemColor.backgroundColor = "grey";
   }
 };
-
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage :'en'},'translater');
-}
